636b7e6669bba82412e86dd5bc7c5d7336170695
twinh <twinhuang@qq.com>
2019-02-11 09:58:39 +0800

    refactor: 更新grid

--- resources/views/admin/shippingTpls/edit.php
+++ resources/views/admin/shippingTpls/edit.php
@@ -10,7 +10,7 @@
 <!-- /.page-header -->
 
 <div class="row">
-  <div class="col-xs-12">
+  <div class="col-12">
     <!-- PAGE detail BEGINS -->
     <form class="js-shipping-tpl-form form-horizontal" method="post" role="form"
       action="<?= $url('admin/shipping-tpls/' . $shippingTpl->getFormAction()) ?>">
@@ -63,7 +63,7 @@
       <input type="hidden" name="id" id="id">
 
       <div class="clearfix form-actions form-group">
-        <div class="col-lg-offset-2">
+        <div class="offset-lg-2">
           <button class="btn btn-primary" type="submit">
             <i class="fa fa-check bigger-110"></i>
             提交
