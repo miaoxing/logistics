fc141c83106817901eef6874c9043149aa632b5e
twinh <twinhuang@qq.com>
2019-02-20 17:31:53 +0800

    refactoring: deparam => unparam

--- resources/views/admin/shippingTpls/index.php
+++ resources/views/admin/shippingTpls/index.php
@@ -48,7 +48,7 @@
 
 <?= $block->js() ?>
 <script>
-  require(['plugins/admin/js/data-table', 'form', 'jquery-deparam'], function () {
+  require(['plugins/admin/js/data-table', 'form', 'jquery-unparam'], function () {
     var recordTable = $('#record-table').dataTable({
       ajax: {
         url: $.queryUrl('admin/shipping-tpls.json')
