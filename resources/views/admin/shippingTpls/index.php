2b54ff4e23b9360d01460b5762e88421fda0529a
twinh <twinhuang@qq.com>
2019-02-20 11:23:09 +0800

    refactoring: data-table 迁移

--- resources/views/admin/shippingTpls/index.php
+++ resources/views/admin/shippingTpls/index.php
@@ -48,7 +48,7 @@
 
 <?= $block->js() ?>
 <script>
-  require(['dataTable', 'form', 'jquery-deparam'], function () {
+  require(['plugins/admin/js/data-table', 'form', 'jquery-deparam'], function () {
     var recordTable = $('#record-table').dataTable({
       ajax: {
         url: $.queryUrl('admin/shipping-tpls.json')
