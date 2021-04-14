3951196b299dd8da577ed44c97d6920513f72f52
twinh <twinhuang@qq.com>
2021-04-15 00:35:17 +0800

    feat: 增加创建页面测试数据的 Seeder

---
+++ src/Seeder/V20210415002226CreateLogistics.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Miaoxing\Logistics\Seeder;
+
+use Miaoxing\Plugin\Seeder\BaseSeeder;
+
+class V20210415002226CreateLogistics extends BaseSeeder
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+    }
+}
