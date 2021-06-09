bc94266b9f2c3253e5e0ade2d188f788cec858c1
twinh <twinhuang@qq.com>
2021-06-09 17:45:40 +0800

    feat(logistics): 增加根据城市获取匹配运费规则的方法

--- src/Service/ShippingTplModel.php
+++ src/Service/ShippingTplModel.php
@@ -8,6 +8,7 @@ use Miaoxing\Plugin\Model\HasAppIdTrait;
 use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Plugin\Model\ReqQueryTrait;
 use Miaoxing\Plugin\Model\SoftDeleteTrait;
+use Miaoxing\Region\Service\RegionModel;
 
 /**
  * @property ShippingTplRuleModel|ShippingTplRuleModel[] $rules
@@ -34,10 +35,96 @@ class ShippingTplModel extends BaseModel
     ];
 
     /**
+     * 特殊城市与所属"市辖区"ID的对应关系
+     *
+     * @var array
+     * @internal
+     */
+    protected $cityIds = [
+        '北京市' => 110100,
+        '天津市' => 120100,
+        '上海市' => 310100,
+        '重庆市' => 500100,
+    ];
+
+    /**
      * @Relation
      */
     public function rules()
     {
         return $this->hasMany(ShippingTplRuleModel::class);
+    }
+
+    /**
+     * 获取城市和匹配的运费规则
+     *
+     * @param string|int $city 如果为空，则按需识别出城市
+     * @return array 返回数组包含 city 和 rules
+     */
+    public function getCityAndRules($city = null): array
+    {
+        if ($this->isFreeShipping) {
+            // 包邮无需加载规则
+            $rules = [];
+        } elseif (1 === $this->rules->count()) {
+            // 只有默认规则，直接返回
+            $rules = $this->rules;
+        } else {
+            // 定位城市并根据城市过滤出匹配的规则
+            $city || $city = wei()->lbs->getIpInfo()['city'];
+            $rules = $this->getRulesByCity($city);
+        }
+
+        return [
+            'city' => $city,
+            'rules' => $rules,
+        ];
+    }
+
+    /**
+     * 根据城市编号或名称,获取匹配的运费规则
+     *
+     * @param int|string $cityId 城市编号或名称,如上海市辖区310000,深圳440300
+     * @return ShippingTplRuleModel|ShippingTplRuleModel[]
+     */
+    public function getRulesByCity($cityId = null): ShippingTplRuleModel
+    {
+        // 如果不是数字ID,查表转换为数字ID
+        if ($cityId && !is_numeric($cityId)) {
+            // 如果是特殊城市,如上海市,转换为上海市"市辖区"的ID
+            if (isset($this->cityIds[$cityId])) {
+                $cityId = $this->cityIds[$cityId];
+            } else {
+                $region = RegionModel::select('id')->findBy('name', $cityId);
+                if ($region) {
+                    $cityId = $region['id'];
+                }
+            }
+        }
+
+        $serviceRules = [];
+        foreach ($this->rules as $rule) {
+            $serviceRules[$rule->serviceId][] = $rule;
+        }
+
+        $matches = [];
+        $default = null;
+        foreach ($serviceRules as $serviceId => $rules) {
+            foreach ($rules as $rule) {
+                if ($rule->isDefault) {
+                    $default = $rule;
+                }
+                if (in_array($cityId, $rule->regionIds, true)) {
+                    $matches[$serviceId] = $rule;
+                    break;
+                }
+            }
+            // 没有匹配的地区,使用默认
+            if (!isset($matches[$serviceId])) {
+                $matches[$serviceId] = $default;
+            }
+        }
+
+        return ShippingTplRuleModel::newColl(array_values($matches));
     }
 }
