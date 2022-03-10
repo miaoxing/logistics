3f82fa7d8ac316ae1e1486f28fdad265e0c515a2
twinh <twinhuang@qq.com>
2022-03-10 10:07:00 +0800

    refactor(logistics): 初始化数据加入到 Seeder 中

--- src/Seeder/V20210415002226CreateLogistics.php
+++ src/Seeder/V20210415002226CreateLogistics.php
@@ -2,6 +2,8 @@
 
 namespace Miaoxing\Logistics\Seeder;
 
+use Miaoxing\Logistics\Service\ShippingTplModel;
+use Miaoxing\Logistics\Service\ShippingTplRuleModel;
 use Miaoxing\Plugin\Seeder\BaseSeeder;
 
 class V20210415002226CreateLogistics extends BaseSeeder
@@ -11,5 +13,25 @@ class V20210415002226CreateLogistics extends BaseSeeder
      */
     public function run()
     {
+        ShippingTplModel::saveAttributes([
+            'serviceIds' => '1',
+            'name' => '包邮',
+            'isFreeShipping' => true,
+        ]);
+
+        $shippingTpl = ShippingTplModel::saveAttributes([
+            'serviceIds' => '1',
+            'name' => '按件 首件10 加件8',
+            'isFreeShipping' => false,
+        ]);
+
+        ShippingTplRuleModel::save([
+            'shippingTplId' => $shippingTpl->id,
+            'isDefault' => true,
+            'startAmount' => 1,
+            'startFee' => 10,
+            'addAmount' => 1,
+            'addFee' => 10,
+        ]);
     }
 }
