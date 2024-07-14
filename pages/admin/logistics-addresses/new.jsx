8fcfbc93d06fe6535ed2fec23fec8cc40978bf09
twinh <twinhuang@qq.com>
2024-07-14 15:47:05 +0800

    feat(u): 调整页面内容为卡片视图

--- pages/admin/logistics-addresses/new.jsx
+++ pages/admin/logistics-addresses/new.jsx
@@ -1,16 +1,17 @@
 /**
  * @share [id]/edit
  */
-import {CListBtn} from '@mxjs/a-clink';
-import {Page, PageActions} from '@mxjs/a-page';
-import {Form, FormAction, FormItem} from '@mxjs/a-form';
+import { CListBtn } from '@mxjs/a-clink';
+import { Page, PageActions } from '@mxjs/a-page';
+import { Form, FormAction, FormItem } from '@mxjs/a-form';
 import RegionCascader from '@mxjs/a-region-cascader';
-import {Checkbox} from 'antd';
+import { Checkbox } from 'antd';
 import $ from 'miaoxing';
-import {FormItemSort} from '@miaoxing/admin';
+import { FormItemSort } from '@miaoxing/admin';
 import Input from '@mxjs/a-input';
-import {useState} from 'react';
+import { useState } from 'react';
 import useAsyncEffect from 'use-async-effect';
+import { Section } from '@mxjs/a-section';
 
 const New = () => {
   const [types, setTypes] = useState([]);
@@ -46,57 +47,57 @@ const New = () => {
           return values;
         }}
       >
-        <FormItem label="联系人姓名" name="name" required>
-          <Input maxLength={16}/>
-        </FormItem>
+        <Section>
+          <FormItem label="联系人姓名" name="name" required>
+            <Input maxLength={16}/>
+          </FormItem>
 
-        <FormItem label="联系人电话" name="phone" required>
-          <Input maxLength={16}/>
-        </FormItem>
+          <FormItem label="联系人电话" name="phone" required>
+            <Input maxLength={16}/>
+          </FormItem>
 
-        <FormItem label="地区" name="regionIds" required
-          rules={[
-            {
-              validator: async (rule, value) => {
-                if (value && value.length !== 3) {
-                  throw new Error('请选择地区到第三级');
-                }
-              },
-            },
-          ]}
-        >
-          <RegionCascader parentId="中国" url="regions" fieldNames={{
-            label: 'shortName',
-            value: 'id',
-            children: 'children',
-          }}/>
-        </FormItem>
+          <FormItem label="地区" name="regionIds" required
+                    rules={[
+                      {
+                        validator: async (rule, value) => {
+                          if (value && value.length !== 3) {
+                            throw new Error('请选择地区到第三级');
+                          }
+                        },
+                      },
+                    ]}
+          >
+            <RegionCascader parentId="中国" url="regions" fieldNames={{
+              label: 'shortName',
+              value: 'id',
+              children: 'children',
+            }}/>
+          </FormItem>
 
-        <FormItem label="详细地址" name="address" required>
-          <Input maxLength={255}/>
-        </FormItem>
+          <FormItem label="详细地址" name="address" required>
+            <Input maxLength={255}/>
+          </FormItem>
 
-        <FormItem label="邮编" name="postalCode">
-          <Input maxLength={8}/>
-        </FormItem>
+          <FormItem label="邮编" name="postalCode">
+            <Input maxLength={8}/>
+          </FormItem>
 
-        <FormItem label="使用场景" name="types">
-          <Checkbox.Group>
-            {types.map(type => (
-              <Checkbox key={type.id} value={type.id}>
-                {type.name}
-              </Checkbox>
-            ))}
-          </Checkbox.Group>
-        </FormItem>
+          <FormItem label="使用场景" name="types">
+            <Checkbox.Group>
+              {types.map(type => (
+                <Checkbox key={type.id} value={type.id}>
+                  {type.name}
+                </Checkbox>
+              ))}
+            </Checkbox.Group>
+          </FormItem>
 
-        <FormItem label="备注" name="remark">
-          <Input.TextArea maxLength={255}/>
-        </FormItem>
+          <FormItem label="备注" name="remark">
+            <Input.TextArea maxLength={255}/>
+          </FormItem>
 
-        <FormItemSort/>
-
-        <FormItem name="id" type="hidden"/>
+          <FormItemSort/>
+        </Section>
 
         <FormAction/>
       </Form>
--- pages/admin/shipping-tpls/new.jsx
+++ pages/admin/shipping-tpls/new.jsx
@@ -18,6 +18,7 @@ import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
 import { FormItemSort, InputPrice } from '@miaoxing/admin';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+import { Section } from '@mxjs/a-section';
 
 // 默认的物流服务编号，即"快递"
 const DEFAULT_SERVICE_ID = 1;
@@ -145,164 +146,165 @@ const New = () => {
           const unit = valuationType === 1 ? '件' : ' kg ';
 
           return <>
-            <FormItem label="名称" name="name" required/>
+            <Section>
+              <FormItem label="名称" name="name" required/>
 
-            <FormItem label="是否包邮" name="isFreeShipping" valuePropName="checked">
-              <Switch/>
-            </FormItem>
-
-            {!isFreeShipping && <>
-              <FormItem label="计价方式" name="valuationType">
-                <Radio.Group>
-                  <Radio value={1}>按件</Radio>
-                  <Radio value={2}>按重量</Radio>
-                </Radio.Group>
+              <FormItem label="是否包邮" name="isFreeShipping" valuePropName="checked">
+                <Switch/>
               </FormItem>
 
-              <FormItem label="运费规则" wrapperCol={{span: 18}}>
-                <FormText className="text-gray-500 mb-2">请设置地区对应的运费，未设置的地区使用默认运费。</FormText>
+              {!isFreeShipping && <>
+                <FormItem label="计价方式" name="valuationType">
+                  <Radio.Group>
+                    <Radio value={1}>按件</Radio>
+                    <Radio value={2}>按重量</Radio>
+                  </Radio.Group>
+                </FormItem>
 
-                <FormList
-                  name="services">
-                  {(fields) => (
-                    <>
-                      {fields.map((field, index) => {
-                        // 每次使用新的对象，以便表格会重新加载
-                        const rules = [...services[index].rules];
+                <FormItem label="运费规则" wrapperCol={{span: 18}}>
+                  <FormText className="text-gray-500 mb-2">请设置地区对应的运费，未设置的地区使用默认运费。</FormText>
 
-                        // 增加规则
-                        const addRule = (e) => {
-                          e.preventDefault();
-                          services[index].rules.push(createRule());
-                          form.current.setFieldsValue({
-                            services: services,
-                          });
-                        };
+                  <FormList
+                    name="services">
+                    {(fields) => (
+                      <>
+                        {fields.map((field, index) => {
+                          // 每次使用新的对象，以便表格会重新加载
+                          const rules = [...services[index].rules];
 
-                        // 删除规则
-                        const deleteRule = (ruleIndex, e) => {
-                          e.preventDefault();
-                          $.confirm('删除后将不能还原，确认删除？', result => {
-                            if (!result) {
-                              return;
-                            }
-
-                            services[index].rules.splice(ruleIndex, 1);
+                          // 增加规则
+                          const addRule = (e) => {
+                            e.preventDefault();
+                            services[index].rules.push(createRule());
                             form.current.setFieldsValue({
                               services: services,
                             });
-                          });
-                        };
+                          };
 
-                        return <Table
-                          key={field.key}
-                          columns={[
-                            {
-                              title: '地区',
-                              dataIndex: 'regionIds',
-                              className: 'align-top',
-                              render: (cell, row, ruleIndex) => {
-                                if (row.isDefault) {
-                                  return <FormText>默认</FormText>;
-                                }
+                          // 删除规则
+                          const deleteRule = (ruleIndex, e) => {
+                            e.preventDefault();
+                            $.confirm('删除后将不能还原，确认删除？', result => {
+                              if (!result) {
+                                return;
+                              }
 
-                                // 找到其他规则已选中的地区
-                                let otherRegionIds = [];
-                                rules.forEach((rule, index) => {
-                                  if (index !== ruleIndex) {
-                                    otherRegionIds = otherRegionIds.concat(rule.regionIds);
+                              services[index].rules.splice(ruleIndex, 1);
+                              form.current.setFieldsValue({
+                                services: services,
+                              });
+                            });
+                          };
+
+                          return <Table
+                            key={field.key}
+                            columns={[
+                              {
+                                title: '地区',
+                                dataIndex: 'regionIds',
+                                className: 'align-top',
+                                render: (cell, row, ruleIndex) => {
+                                  if (row.isDefault) {
+                                    return <FormText>默认</FormText>;
                                   }
-                                });
 
-                                // 禁用已选中的地区
-                                const treeData = regions.map(region => {
-                                  const disabled = otherRegionIds.includes(region.value);
-                                  return {
-                                    ...region,
-                                    disabled,
-                                    children: region.children.map(child => {
-                                      return {
-                                        ...child,
-                                        disabled: disabled || otherRegionIds.includes(child.value),
-                                      };
-                                    }),
-                                  };
-                                });
+                                  // 找到其他规则已选中的地区
+                                  let otherRegionIds = [];
+                                  rules.forEach((rule, index) => {
+                                    if (index !== ruleIndex) {
+                                      otherRegionIds = otherRegionIds.concat(rule.regionIds);
+                                    }
+                                  });
 
-                                return (
-                                  <FormItem name={[index, 'rules', ruleIndex, 'regionIds']} required
-                                            style={{marginBottom: 0}}
-                                  >
-                                    <TreeSelect treeData={treeData} treeCheckable
-                                                showCheckedStrategy={TreeSelect.SHOW_PARENT} placeholder="请选择"
-                                                treeNodeFilterProp="title"
-                                    />
-                                  </FormItem>
-                                );
+                                  // 禁用已选中的地区
+                                  const treeData = regions.map(region => {
+                                    const disabled = otherRegionIds.includes(region.value);
+                                    return {
+                                      ...region,
+                                      disabled,
+                                      children: region.children.map(child => {
+                                        return {
+                                          ...child,
+                                          disabled: disabled || otherRegionIds.includes(child.value),
+                                        };
+                                      }),
+                                    };
+                                  });
+
+                                  return (
+                                    <FormItem name={[index, 'rules', ruleIndex, 'regionIds']} required
+                                              style={{marginBottom: 0}}
+                                    >
+                                      <TreeSelect treeData={treeData} treeCheckable
+                                                  showCheckedStrategy={TreeSelect.SHOW_PARENT} placeholder="请选择"
+                                                  treeNodeFilterProp="title"
+                                      />
+                                    </FormItem>
+                                  );
+                                },
                               },
-                            },
-                            {
-                              title: '运费',
-                              dataIndex: 'startAmount',
-                              align: 'center',
-                              width: 460,
-                              className: 'align-top',
-                              render: (value, row, ruleIndex) => {
-                                return <div className="flex align-start justify-between">
-                                  <NumberFormItem name={[index, 'rules', ruleIndex, 'startAmount']}/>
-                                  <FormText>{unit}内</FormText>
-                                  <FeeFormItem name={[index, 'rules', ruleIndex, 'startFee']}/>
-                                  <FormText>元，每增加</FormText>
-                                  <NumberFormItem name={[index, 'rules', ruleIndex, 'addAmount']}/>
-                                  <FormText>{unit}</FormText>
-                                  <FeeFormItem name={[index, 'rules', ruleIndex, 'addFee']}/>
-                                  <FormText>元</FormText>
-                                </div>;
+                              {
+                                title: '运费',
+                                dataIndex: 'startAmount',
+                                align: 'center',
+                                width: 460,
+                                className: 'align-top',
+                                render: (value, row, ruleIndex) => {
+                                  return <div className="flex align-start justify-between">
+                                    <NumberFormItem name={[index, 'rules', ruleIndex, 'startAmount']}/>
+                                    <FormText>{unit}内</FormText>
+                                    <FeeFormItem name={[index, 'rules', ruleIndex, 'startFee']}/>
+                                    <FormText>元，每增加</FormText>
+                                    <NumberFormItem name={[index, 'rules', ruleIndex, 'addAmount']}/>
+                                    <FormText>{unit}</FormText>
+                                    <FeeFormItem name={[index, 'rules', ruleIndex, 'addFee']}/>
+                                    <FormText>元</FormText>
+                                  </div>;
+                                },
                               },
-                            },
-                            {
-                              title: '操作',
-                              dataIndex: 'id',
-                              align: 'center',
-                              width: 100,
-                              className: 'align-top',
-                              render: (value, row, ruleIndex) => {
-                                return row.isDefault ? <span title="默认地区不能删除">-</span> : (
-                                  <Typography.Link type="danger" onClick={deleteRule.bind(this, ruleIndex)} href="#"
-                                                   title="删除">
-                                    <DeleteOutlined/>
-                                  </Typography.Link>
-                                );
+                              {
+                                title: '操作',
+                                dataIndex: 'id',
+                                align: 'center',
+                                width: 100,
+                                className: 'align-top',
+                                render: (value, row, ruleIndex) => {
+                                  return row.isDefault ? <span title="默认地区不能删除">-</span> : (
+                                    <Typography.Link type="danger" onClick={deleteRule.bind(this, ruleIndex)} href="#"
+                                                     title="删除">
+                                      <DeleteOutlined/>
+                                    </Typography.Link>
+                                  );
+                                },
                               },
-                            },
-                          ]}
-                          bordered
-                          dataSource={rules}
-                          rowKey={(record) => record.id || record.key}
-                          footer={() => {
-                            return <a onClick={addRule} href="#">
-                              <PlusOutlined/> 为指定地区设置运费
-                            </a>;
-                          }}
-                          pagination={false}
-                        />;
-                      })}
+                            ]}
+                            bordered
+                            dataSource={rules}
+                            rowKey={(record) => record.id || record.key}
+                            footer={() => {
+                              return <a onClick={addRule} href="#">
+                                <PlusOutlined/> 为指定地区设置运费
+                              </a>;
+                            }}
+                            pagination={false}
+                          />;
+                        })}
 
-                    </>
-                  )}
-                </FormList>
-              </FormItem>
-            </>}
+                      </>
+                    )}
+                  </FormList>
+                </FormItem>
+              </>}
 
-            <FormItemSort extra={
-              <>
-                大的显示在前面，按从大到小排列。<br/>
-                创建商品时，将默认选中最大顺序的运费模板。
-              </>
-            }
-            />
+              <FormItemSort extra={
+                <>
+                  大的显示在前面，按从大到小排列。<br/>
+                  创建商品时，将默认选中最大顺序的运费模板。
+                </>
+              }
+              />
 
-            <FormItem name="id" type="hidden"/>
+            </Section>
             <FormAction/>
           </>;
         }}
